b2fc1144656c4553046fbf4beb2bde6bd4cd2a08
futsuuu <futsuuu123@gmail.com>
2025-11-28 20:36:17 +0900

    refactor

--- src/kernel.zig
+++ src/kernel.zig
@@ -75,7 +75,7 @@ fn procAEntry() void {
     log.debug("starting process A", .{});
     while (true) {
         sbi.console.putChar('A');
-        switchContext(&proc_a.sp, &proc_b.sp);
+        proc_a.switchContext(proc_b);
         delay();
     }
 }
@@ -84,232 +84,121 @@ fn procBEntry() void {
     log.debug("starting process B", .{});
     while (true) {
         sbi.console.putChar('B');
-        switchContext(&proc_b.sp, &proc_a.sp);
+        proc_b.switchContext(proc_a);
         delay();
     }
-}
-
-fn kernelEntry() align(4) callconv(.naked) noreturn {
-    asm volatile (
-        \\ csrw sscratch, sp
-        \\ addi sp, sp, -4 * 31
-        \\ sw ra,  4 * 0(sp)
-        \\ sw gp,  4 * 1(sp)
-        \\ sw tp,  4 * 2(sp)
-        \\ sw t0,  4 * 3(sp)
-        \\ sw t1,  4 * 4(sp)
-        \\ sw t2,  4 * 5(sp)
-        \\ sw t3,  4 * 6(sp)
-        \\ sw t4,  4 * 7(sp)
-        \\ sw t5,  4 * 8(sp)
-        \\ sw t6,  4 * 9(sp)
-        \\ sw a0,  4 * 10(sp)
-        \\ sw a1,  4 * 11(sp)
-        \\ sw a2,  4 * 12(sp)
-        \\ sw a3,  4 * 13(sp)
-        \\ sw a4,  4 * 14(sp)
-        \\ sw a5,  4 * 15(sp)
-        \\ sw a6,  4 * 16(sp)
-        \\ sw a7,  4 * 17(sp)
-        \\ sw s0,  4 * 18(sp)
-        \\ sw s1,  4 * 19(sp)
-        \\ sw s2,  4 * 20(sp)
-        \\ sw s3,  4 * 21(sp)
-        \\ sw s4,  4 * 22(sp)
-        \\ sw s5,  4 * 23(sp)
-        \\ sw s6,  4 * 24(sp)
-        \\ sw s7,  4 * 25(sp)
-        \\ sw s8,  4 * 26(sp)
-        \\ sw s9,  4 * 27(sp)
-        \\ sw s10, 4 * 28(sp)
-        \\ sw s11, 4 * 29(sp)
-        \\
-        \\ csrr a0, sscratch
-        \\ sw a0,  4 * 30(sp)
-        \\
-        \\ mv a0, sp
-        \\ call handleTrap
-        \\
-        \\ lw ra,  4 * 0(sp)
-        \\ lw gp,  4 * 1(sp)
-        \\ lw tp,  4 * 2(sp)
-        \\ lw t0,  4 * 3(sp)
-        \\ lw t1,  4 * 4(sp)
-        \\ lw t2,  4 * 5(sp)
-        \\ lw t3,  4 * 6(sp)
-        \\ lw t4,  4 * 7(sp)
-        \\ lw t5,  4 * 8(sp)
-        \\ lw t6,  4 * 9(sp)
-        \\ lw a0,  4 * 10(sp)
-        \\ lw a1,  4 * 11(sp)
-        \\ lw a2,  4 * 12(sp)
-        \\ lw a3,  4 * 13(sp)
-        \\ lw a4,  4 * 14(sp)
-        \\ lw a5,  4 * 15(sp)
-        \\ lw a6,  4 * 16(sp)
-        \\ lw a7,  4 * 17(sp)
-        \\ lw s0,  4 * 18(sp)
-        \\ lw s1,  4 * 19(sp)
-        \\ lw s2,  4 * 20(sp)
-        \\ lw s3,  4 * 21(sp)
-        \\ lw s4,  4 * 22(sp)
-        \\ lw s5,  4 * 23(sp)
-        \\ lw s6,  4 * 24(sp)
-        \\ lw s7,  4 * 25(sp)
-        \\ lw s8,  4 * 26(sp)
-        \\ lw s9,  4 * 27(sp)
-        \\ lw s10, 4 * 28(sp)
-        \\ lw s11, 4 * 29(sp)
-        \\ lw sp,  4 * 30(sp)
-        \\ sret
-    );
-}
-
-export fn handleTrap(frame: *TrapFrame) void {
-    _ = frame;
-    const scause = readCsr(.scause);
-    const stval = readCsr(.stval);
-    const user_pc = readCsr(.sepc);
-    std.debug.panic("unexpected trap: scause={x}, stval={x}, sepc={x}", .{ scause, stval, user_pc });
-}
-
-const TrapFrame = packed struct {
-    ra: usize,
-    gp: usize,
-    tp: usize,
-    t0: usize,
-    t1: usize,
-    t2: usize,
-    t3: usize,
-    t4: usize,
-    t5: usize,
-    t6: usize,
-    a0: usize,
-    a1: usize,
-    a2: usize,
-    a3: usize,
-    a4: usize,
-    a5: usize,
-    a6: usize,
-    a7: usize,
-    s0: usize,
-    s1: usize,
-    s2: usize,
-    s3: usize,
-    s4: usize,
-    s5: usize,
-    s6: usize,
-    s7: usize,
-    s8: usize,
-    s9: usize,
-    s10: usize,
-    s11: usize,
-    sp: usize,
-};
-
-inline fn readCsr(comptime reg: @Type(.enum_literal)) usize {
-    return asm volatile ("csrr %[ret], " ++ @tagName(reg)
-        : [ret] "=r" (-> usize),
-    );
-}
-
-inline fn writeCsr(comptime reg: @Type(.enum_literal), value: usize) void {
-    asm volatile ("csrw " ++ @tagName(reg) ++ ", %[value]"
-        :
-        : [value] "r" (value),
-    );
 }
 
 const Process = struct {
     state: State = .unused,
     pid: usize = undefined,
-    sp: usize = undefined,
-    stack: [8192]u8 = undefined,
+    sp: *u8 = undefined,
+    stack: [8192]u8 align(@alignOf(usize)) = undefined,
 
     const State = enum { unused, runnable };
+
+    const Self = @This();
 
     const procs_max = 8;
     var procs: [procs_max]Process = .{Process{}} ** procs_max;
 
-    fn create(pc: usize) *Process {
-        var proc: *Process = &procs[0];
-        var pid: usize = 0;
-        for (&procs) |*p| {
-            pid += 1;
-            if (p.state == .unused) {
-                proc = p;
-                break;
-            }
-        }
-        if (proc.state != .unused) {
+    fn create(pc: usize) *Self {
+        const proc: *Process = blk: for (&procs, 1..) |*proc, pid| {
+            if (proc.state != .unused) continue;
+            proc.pid = pid;
+            break :blk proc;
+        } else {
             @panic("no free process slots");
-        }
-        var sp: usize = @intFromPtr(&proc.stack[proc.stack.len - 1]);
-        inline for ([_][]const u8 {
-            "s11",
-            "s10",
-            "s9",
-            "s8",
-            "s7",
-            "s6",
-            "s5",
-            "s4",
-            "s3",
-            "s2",
-            "s1",
-            "s0",
-        }) |_| {
-            @as(*usize, @ptrFromInt(sp)).* = 0;
-            sp -= @sizeOf(usize);
-        }
-        @as(*usize, @ptrFromInt(sp)).* = pc; // ra
-        proc.pid = pid;
+        };
         proc.state = .runnable;
-        proc.sp = sp;
+        //  | ...
+        //  |-----> proc.sp
+        //  | pc    // -> ra
+        //  |-----
+        //  | 0     // -> s0
+        //  |-----
+        //  | 0     // -> s1 .. s10
+        //  |-----
+        //  | 0     // -> s11
+        //  `-----
+        const casted_stack = blk: {
+            var ptr: [*]usize = @ptrCast(&proc.stack);
+            break :blk ptr[0 .. proc.stack.len * @sizeOf(u8) / @sizeOf(usize)];
+        };
+        casted_stack[casted_stack.len - 13] = pc;
+        for (casted_stack.len - 12..casted_stack.len) |i| {
+            casted_stack[i] = 0;
+        }
+        proc.sp = @ptrCast(&casted_stack[casted_stack.len - 13]);
         return proc;
     }
-};
 
-noinline fn switchContext(prev: *u32, next: *u32) void {
-    asm volatile (
-        \\ addi sp, sp, -13 * 4
-        \\ sw ra,  0  * 4(sp)
-        \\ sw s0,  1  * 4(sp)
-        \\ sw s1,  2  * 4(sp)
-        \\ sw s2,  3  * 4(sp)
-        \\ sw s3,  4  * 4(sp)
-        \\ sw s4,  5  * 4(sp)
-        \\ sw s5,  6  * 4(sp)
-        \\ sw s6,  7  * 4(sp)
-        \\ sw s7,  8  * 4(sp)
-        \\ sw s8,  9  * 4(sp)
-        \\ sw s9,  10 * 4(sp)
-        \\ sw s10, 11 * 4(sp)
-        \\ sw s11, 12 * 4(sp)
-        \\
-        \\ sw sp, (%[prev])
-        \\ lw sp, (%[next])
-        \\
-        \\ lw ra,  0  * 4(sp)
-        \\ lw s0,  1  * 4(sp)
-        \\ lw s1,  2  * 4(sp)
-        \\ lw s2,  3  * 4(sp)
-        \\ lw s3,  4  * 4(sp)
-        \\ lw s4,  5  * 4(sp)
-        \\ lw s5,  6  * 4(sp)
-        \\ lw s6,  7  * 4(sp)
-        \\ lw s7,  8  * 4(sp)
-        \\ lw s8,  9  * 4(sp)
-        \\ lw s9,  10 * 4(sp)
-        \\ lw s10, 11 * 4(sp)
-        \\ lw s11, 12 * 4(sp)
-        \\ addi sp, sp, 13 * 4
-        \\ ret
-        :
-        : [prev] "{a0}" (prev),
-          [next] "{a1}" (next),
-    );
-}
+    noinline fn switchContext(self: *Self, next: *Self) void {
+        asm volatile (
+            \\
+            // | ...
+            // |-----> sp
+            // | <- ra
+            // |-----
+            // | <- s0
+            // |-----
+            // | <- s1 .. s10
+            // |-----
+            // | <- s11
+            // |-----> prev sp
+            // | ...
+            \\ addi sp, sp, -13 * 4
+            \\ sw ra,  0  * 4(sp)
+            \\ sw s0,  1  * 4(sp)
+            \\ sw s1,  2  * 4(sp)
+            \\ sw s2,  3  * 4(sp)
+            \\ sw s3,  4  * 4(sp)
+            \\ sw s4,  5  * 4(sp)
+            \\ sw s5,  6  * 4(sp)
+            \\ sw s6,  7  * 4(sp)
+            \\ sw s7,  8  * 4(sp)
+            \\ sw s8,  9  * 4(sp)
+            \\ sw s9,  10 * 4(sp)
+            \\ sw s10, 11 * 4(sp)
+            \\ sw s11, 12 * 4(sp)
+
+            // self.sp = sp
+            \\ sw sp, (%[self])
+            // sp = next.sp
+            \\ lw sp, (%[next])
+
+            // | ...
+            // |-----> next.sp
+            // | -> ra
+            // |-----
+            // | -> s0
+            // |-----
+            // | -> s1 .. s10
+            // |-----
+            // | -> s11
+            // |-----> sp
+            // | ...
+            \\ lw ra,  0  * 4(sp)
+            \\ lw s0,  1  * 4(sp)
+            \\ lw s1,  2  * 4(sp)
+            \\ lw s2,  3  * 4(sp)
+            \\ lw s3,  4  * 4(sp)
+            \\ lw s4,  5  * 4(sp)
+            \\ lw s5,  6  * 4(sp)
+            \\ lw s6,  7  * 4(sp)
+            \\ lw s7,  8  * 4(sp)
+            \\ lw s8,  9  * 4(sp)
+            \\ lw s9,  10 * 4(sp)
+            \\ lw s10, 11 * 4(sp)
+            \\ lw s11, 12 * 4(sp)
+            \\ addi sp, sp, 13 * 4
+            \\ ret
+            :
+            : [self] "r" (&self.sp),
+              [next] "r" (&next.sp),
+        );
+    }
+};
 
 const ram = struct {
     const free_ram = @extern([*]u8, .{ .name = "__free_ram" });
@@ -446,4 +335,135 @@ const sbi = struct {
             return written;
         }
     };
+};
+
+inline fn readCsr(comptime reg: @Type(.enum_literal)) usize {
+    return asm volatile ("csrr %[ret], " ++ @tagName(reg)
+        : [ret] "=r" (-> usize),
+    );
+}
+
+inline fn writeCsr(comptime reg: @Type(.enum_literal), value: usize) void {
+    asm volatile ("csrw " ++ @tagName(reg) ++ ", %[value]"
+        :
+        : [value] "r" (value),
+    );
+}
+
+fn kernelEntry() align(4) callconv(.naked) noreturn {
+    asm volatile (
+        \\ csrw sscratch, sp
+        \\ addi sp, sp, -4 * 31
+        \\ sw ra,  4 * 0(sp)
+        \\ sw gp,  4 * 1(sp)
+        \\ sw tp,  4 * 2(sp)
+        \\ sw t0,  4 * 3(sp)
+        \\ sw t1,  4 * 4(sp)
+        \\ sw t2,  4 * 5(sp)
+        \\ sw t3,  4 * 6(sp)
+        \\ sw t4,  4 * 7(sp)
+        \\ sw t5,  4 * 8(sp)
+        \\ sw t6,  4 * 9(sp)
+        \\ sw a0,  4 * 10(sp)
+        \\ sw a1,  4 * 11(sp)
+        \\ sw a2,  4 * 12(sp)
+        \\ sw a3,  4 * 13(sp)
+        \\ sw a4,  4 * 14(sp)
+        \\ sw a5,  4 * 15(sp)
+        \\ sw a6,  4 * 16(sp)
+        \\ sw a7,  4 * 17(sp)
+        \\ sw s0,  4 * 18(sp)
+        \\ sw s1,  4 * 19(sp)
+        \\ sw s2,  4 * 20(sp)
+        \\ sw s3,  4 * 21(sp)
+        \\ sw s4,  4 * 22(sp)
+        \\ sw s5,  4 * 23(sp)
+        \\ sw s6,  4 * 24(sp)
+        \\ sw s7,  4 * 25(sp)
+        \\ sw s8,  4 * 26(sp)
+        \\ sw s9,  4 * 27(sp)
+        \\ sw s10, 4 * 28(sp)
+        \\ sw s11, 4 * 29(sp)
+        \\
+        \\ csrr a0, sscratch
+        \\ sw a0,  4 * 30(sp)
+        \\
+        \\ mv a0, sp
+        \\ call handleTrap
+        \\
+        \\ lw ra,  4 * 0(sp)
+        \\ lw gp,  4 * 1(sp)
+        \\ lw tp,  4 * 2(sp)
+        \\ lw t0,  4 * 3(sp)
+        \\ lw t1,  4 * 4(sp)
+        \\ lw t2,  4 * 5(sp)
+        \\ lw t3,  4 * 6(sp)
+        \\ lw t4,  4 * 7(sp)
+        \\ lw t5,  4 * 8(sp)
+        \\ lw t6,  4 * 9(sp)
+        \\ lw a0,  4 * 10(sp)
+        \\ lw a1,  4 * 11(sp)
+        \\ lw a2,  4 * 12(sp)
+        \\ lw a3,  4 * 13(sp)
+        \\ lw a4,  4 * 14(sp)
+        \\ lw a5,  4 * 15(sp)
+        \\ lw a6,  4 * 16(sp)
+        \\ lw a7,  4 * 17(sp)
+        \\ lw s0,  4 * 18(sp)
+        \\ lw s1,  4 * 19(sp)
+        \\ lw s2,  4 * 20(sp)
+        \\ lw s3,  4 * 21(sp)
+        \\ lw s4,  4 * 22(sp)
+        \\ lw s5,  4 * 23(sp)
+        \\ lw s6,  4 * 24(sp)
+        \\ lw s7,  4 * 25(sp)
+        \\ lw s8,  4 * 26(sp)
+        \\ lw s9,  4 * 27(sp)
+        \\ lw s10, 4 * 28(sp)
+        \\ lw s11, 4 * 29(sp)
+        \\ lw sp,  4 * 30(sp)
+        \\ sret
+    );
+}
+
+export fn handleTrap(frame: *TrapFrame) void {
+    _ = frame;
+    const scause = readCsr(.scause);
+    const stval = readCsr(.stval);
+    const user_pc = readCsr(.sepc);
+    std.debug.panic("unexpected trap: scause={x}, stval={x}, sepc={x}", .{ scause, stval, user_pc });
+}
+
+const TrapFrame = packed struct {
+    ra: usize,
+    gp: usize,
+    tp: usize,
+    t0: usize,
+    t1: usize,
+    t2: usize,
+    t3: usize,
+    t4: usize,
+    t5: usize,
+    t6: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+    s0: usize,
+    s1: usize,
+    s2: usize,
+    s3: usize,
+    s4: usize,
+    s5: usize,
+    s6: usize,
+    s7: usize,
+    s8: usize,
+    s9: usize,
+    s10: usize,
+    s11: usize,
+    sp: usize,
 };
