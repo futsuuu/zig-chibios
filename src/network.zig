d4f595eef1f56246e28853e90bb3f5190949e182
futsuuu <futsuuu123@gmail.com>
2026-06-11 03:37:03 +0900

    refactor

--- src/network.zig
+++ src/network.zig
@@ -3,61 +3,22 @@ const std = @import("std");
 const Be = @import("endian.zig").Big;
 
 pub const MACAddress = extern struct {
-    bytes: [6]u8,
+    octets: [6]u8,
 
+    pub const unspecified: MACAddress = .init(@splat(0x00));
     pub const broadcast: MACAddress = .init(@splat(0xff));
 
     pub fn init(address: [6]u8) MACAddress {
-        return .{ .bytes = address };
+        return .{ .octets = address };
     }
 
     pub fn format(self: MACAddress, writer: *std.Io.Writer) std.Io.Writer.Error!void {
         try writer.print("{X:0>2}:{X:0>2}:{X:0>2}:{X:0>2}:{X:0>2}:{X:0>2}", .{
-            self.bytes[0], self.bytes[1], self.bytes[2],
-            self.bytes[3], self.bytes[4], self.bytes[5],
+            self.octets[0], self.octets[1], self.octets[2],
+            self.octets[3], self.octets[4], self.octets[5],
         });
     }
 };
-
-pub const IPv4Address = extern struct {
-    bytes: [4]u8,
-
-    pub fn init(address: [4]u8) IPv4Address {
-        return .{ .bytes = address };
-    }
-
-    pub fn format(self: IPv4Address, writer: *std.Io.Writer) std.Io.Writer.Error!void {
-        try writer.print("{}.{}.{}.{}", .{
-            self.bytes[0], self.bytes[1], self.bytes[2], self.bytes[3],
-        });
-    }
-};
-
-pub fn buildARPRequest(
-    our_mac: MACAddress,
-    our_ip: IPv4Address,
-    target_ip: IPv4Address,
-) [@sizeOf(EthernetHeader) + @sizeOf(ARPHeader) + @sizeOf(StaticARPBody(.Ethernet, .IPv4))]u8 {
-    const ethernet_header: EthernetHeader = .{
-        .target_mac_address = .broadcast,
-        .source_mac_address = our_mac,
-        .protocol = .fromNative(.ARP),
-    };
-    const arp_header: ARPHeader = .{
-        .hardware = .fromNative(.Ethernet),
-        .protocol = .fromNative(.IPv4),
-        .hardware_address_size = ARPHardwareType.addressSizeHint(.Ethernet).?,
-        .protocol_address_size = EtherType.addressSizeHint(.IPv4).?,
-        .operation = .fromNative(.request),
-    };
-    const arp_body: StaticARPBody(.Ethernet, .IPv4) = .{
-        .source_hardware_address = our_mac.bytes,
-        .source_protocol_address = our_ip.bytes,
-        .target_hardware_address = @splat(0),
-        .target_protocol_address = target_ip.bytes,
-    };
-    return std.mem.toBytes(ethernet_header) ++ std.mem.toBytes(arp_header) ++ std.mem.toBytes(arp_body);
-}
 
 pub const EthernetHeader = extern struct {
     target_mac_address: MACAddress,
@@ -99,11 +60,13 @@ pub fn StaticARPBody(hardware: ARPHardwareType, protocol: EtherType) type {
 
 pub const ARPHardwareType = enum(u16) {
     Ethernet = 1,
+    IEEE_802 = 6,
     _,
 
     fn addressSizeHint(self: ARPHardwareType) ?u8 {
         return switch (self) {
             .Ethernet => 6,
+            .IEEE_802 => null,
             _ => null,
         };
     }
@@ -115,19 +78,60 @@ pub const ARPOperation = enum(u16) {
     _,
 };
 
-pub fn parseARPReply(frame: []const u8, our_ip: IPv4Address) ?MACAddress {
-    const body_size = @sizeOf(StaticARPBody(.Ethernet, .IPv4));
-    const offset = @sizeOf(EthernetHeader) + @sizeOf(ARPHeader);
-    if (frame.len < offset + body_size) return null;
+pub fn buildARPRequest(
+    our_mac: MACAddress,
+    our_ip: IPv4Address,
+    target_ip: IPv4Address,
+) [@sizeOf(EthernetHeader) + @sizeOf(ARPHeader) + @sizeOf(StaticARPBody(.Ethernet, .IPv4))]u8 {
+    const ethernet_header: EthernetHeader = .{
+        .target_mac_address = .broadcast,
+        .source_mac_address = our_mac,
+        .protocol = .fromNative(.ARP),
+    };
+    const arp_header: ARPHeader = .{
+        .hardware = .fromNative(.Ethernet),
+        .protocol = .fromNative(.IPv4),
+        .hardware_address_size = ARPHardwareType.addressSizeHint(.Ethernet).?,
+        .protocol_address_size = EtherType.addressSizeHint(.IPv4).?,
+        .operation = .fromNative(.request),
+    };
+    const arp_body: StaticARPBody(.Ethernet, .IPv4) = .{
+        .source_hardware_address = our_mac.octets,
+        .source_protocol_address = our_ip.octets,
+        .target_hardware_address = MACAddress.unspecified.octets,
+        .target_protocol_address = target_ip.octets,
+    };
+    return std.mem.toBytes(ethernet_header) ++ std.mem.toBytes(arp_header) ++ std.mem.toBytes(arp_body);
+}
 
-    const ethernet_header = std.mem.bytesAsValue(EthernetHeader, frame[0..@sizeOf(EthernetHeader)]);
+pub fn parseARPReply(frame: []const u8, our_ip: IPv4Address) ?MACAddress {
+    var r: std.Io.Reader = .fixed(frame);
+
+    const ethernet_header = r.takeStructPointer(EthernetHeader) catch return null;
     if (ethernet_header.protocol.toNative() != .ARP) return null;
 
-    const arp_header = std.mem.bytesAsValue(ARPHeader, frame[@sizeOf(EthernetHeader)..][0..@sizeOf(ARPHeader)]);
+    const arp_header = r.takeStructPointer(ARPHeader) catch return null;
     if (arp_header.operation.toNative() != .reply) return null;
 
-    const arp_body = std.mem.bytesAsValue(StaticARPBody(.Ethernet, .IPv4), frame[offset..][0..body_size]);
-    if (!std.mem.eql(u8, &arp_body.target_protocol_address, &our_ip.bytes)) return null;
+    const arp_body = r.takeStructPointer(StaticARPBody(.Ethernet, .IPv4)) catch return null;
+    if (!std.mem.eql(u8, &arp_body.target_protocol_address, &our_ip.octets)) return null;
 
     return .init(arp_body.source_hardware_address);
 }
+
+pub const IPv4Address = extern struct {
+    octets: [4]u8,
+
+    pub const unspecified: IPv4Address = .init(@splat(0));
+    pub const broadcast: IPv4Address = .init(@splat(255));
+
+    pub fn init(address: [4]u8) IPv4Address {
+        return .{ .octets = address };
+    }
+
+    pub fn format(self: IPv4Address, writer: *std.Io.Writer) std.Io.Writer.Error!void {
+        try writer.print("{}.{}.{}.{}", .{
+            self.octets[0], self.octets[1], self.octets[2], self.octets[3],
+        });
+    }
+};
