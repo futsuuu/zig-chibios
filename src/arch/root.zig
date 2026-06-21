2ed344beaf27f2e5dd0b622fa68e7c9ea04ff602
futsuuu <futsuuu123@gmail.com>
2026-06-21 11:07:58 +0900

    arch: set MMU type to void if not implemented for the target

--- src/arch/root.zig
+++ src/arch/root.zig
@@ -1,10 +1,15 @@
+const builtin = @import("builtin");
 const std = @import("std");
 
 pub const riscv = @import("riscv.zig");
 pub const Context = riscv.Context;
 pub const barrier = riscv.barrier;
-pub const mmu = riscv.mmu.native;
 pub const trap = riscv.trap;
+pub const mmu = switch (builtin.target.cpu.arch) {
+    .riscv32, .riscv32be => riscv.mmu.sv32,
+    .riscv64, .riscv64be => riscv.mmu.sv39,
+    else => void,
+};
 
 comptime {
     _ = riscv;
